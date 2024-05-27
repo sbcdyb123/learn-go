00fd63aceb57ad4e609c5ae749f58626c19150a7
Fanglong <337956017@qq.com>
2024-05-27 17:05:21 +0800

    reset

--- main.go
+++ main.go
@@ -1,67 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"gocms/app/cmd"
-	"gocms/app/cmd/make"
-	"gocms/bootstrap"
-	btsConig "gocms/config"
-	"gocms/pkg/config"
-	"gocms/pkg/console"
-	"os"
-
-	"github.com/spf13/cobra"
-)
-
-func init() {
-	// 加载 config 目录下的配置信息
-	btsConig.Initialize()
-}
+import "fmt"
 
 func main() {
+	fmt.Println("Hello, World!")
 
-	// 应用的主入口，默认调用 cmd.CmdServe 命令
-	var rootCmd = &cobra.Command{
-		Use:   "gocms",
-		Short: "A simple forum project",
-		Long:  `Default will run "serve" command, you can use "-h" flag to see all subcommands`,
-
-		// rootCmd 的所有子命令都会执行以下代码
-		PersistentPreRun: func(command *cobra.Command, args []string) {
-
-			// 配置初始化，依赖命令行 --env 参数
-			config.InitConfig(cmd.Env)
-
-			// 初始化 Logger
-			bootstrap.SetupLogger()
-
-			// 初始化数据库
-			bootstrap.SetupDB()
-
-			// 初始化 Redis
-			bootstrap.SetupRedis()
-
-			// 初始化缓存
-		},
-	}
-
-	// 注册子命令
-	rootCmd.AddCommand(
-		cmd.CmdServe,
-		cmd.CmdKey,
-		cmd.CmdPlay,
-		make.CmdMake,
-		cmd.CmdMigrate,
-	)
-
-	// 配置默认运行 Web 服务
-	cmd.RegisterDefaultCmd(rootCmd, cmd.CmdServe)
-
-	// 注册全局参数，--env
-	cmd.RegisterGlobalFlags(rootCmd)
-
-	// 执行主命令
-	if err := rootCmd.Execute(); err != nil {
-		console.Exit(fmt.Sprintf("Failed to run app with %v: %s", os.Args, err.Error()))
-	}
 }
