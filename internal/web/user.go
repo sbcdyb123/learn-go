151e0e79c46e54e6720c81d1b1f1d224a9b7d892
Fanglong <337956017@qq.com>
2024-05-30 11:35:50 +0800

    add user

---
+++ internal/web/user.go
@@ -0,0 +1,83 @@
+package web
+
+import (
+	"fmt"
+	regexp "github.com/dlclark/regexp2"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type UserHandler struct {
+	emailExp    *regexp.Regexp
+	passwordExp *regexp.Regexp
+}
+
+func NewUserHandler() *UserHandler {
+	const (
+		emailRegexPattern    = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"
+		passwordRegexPattern = `^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$`
+	)
+	emailExp := regexp.MustCompile(emailRegexPattern, regexp.None)
+	passwordExp := regexp.MustCompile(passwordRegexPattern, regexp.None)
+	return &UserHandler{
+		emailExp:    emailExp,
+		passwordExp: passwordExp,
+	}
+}
+
+func (u *UserHandler) RegisterRoutes(server *gin.Engine) {
+	ug := server.Group("/user")
+	ug.POST("/signup", u.Signup)
+	ug.POST("/login", u.Login)
+	ug.POST("/edit", u.Edit)
+	ug.GET("/profile", u.Profile)
+}
+
+func (u *UserHandler) Signup(c *gin.Context) {
+	type SignUpReq struct {
+		Email           string `json:"email"`
+		ConfirmPassword string `json:"confirmPassword"`
+		Password        string `json:"password"`
+	}
+	var req SignUpReq
+	//bind方法会根据content-type来解析你的数据到req里面
+	//解析错误，直接就会返回4xx错误
+	if err := c.Bind(&req); err != nil {
+		return
+	}
+
+	ok, err := u.emailExp.MatchString(req.Email)
+	//ok, err := regexp.Match(emailRegexPattern, []byte(req.Email))
+	if err != nil {
+		c.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		c.String(http.StatusOK, "邮箱格式错误")
+		return
+	}
+	if req.Password != req.ConfirmPassword {
+		c.String(http.StatusOK, "两次输入的密码不一致")
+		return
+	}
+	ok, err = u.passwordExp.MatchString(req.Password)
+	//ok, err = regexp.Match(passwordRegexPattern, []byte(req.Password))
+	if err != nil {
+		c.String(http.StatusOK, "系统错误")
+		return
+	}
+	if !ok {
+		c.String(http.StatusOK, "密码必须大于8位，且包含大小写字母、数字和特殊符号")
+	}
+	fmt.Printf("req:%+v\n", req)
+	c.String(http.StatusOK, "注册成功")
+}
+func (u *UserHandler) Login(c *gin.Context) {
+
+}
+func (u *UserHandler) Edit(c *gin.Context) {
+
+}
+func (u *UserHandler) Profile(c *gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sbcdyb123/learn-go/internal/web"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-
+	server := gin.Default()
+	u := web.NewUserHandler()
+	u.RegisterRoutes(server)
+	server.Run(":8080") // 监听并在 0.0.0.0:8080 上启动服务
 }
